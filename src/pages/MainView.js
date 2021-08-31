2ccc415d496a7eb643a2d777c10b42be51abb133
Natalia Bedoya Ruiz <44883548+NataliaBedoya@users.noreply.github.com>
2021-08-31 10:51:27 -0500

    Creación de página main y not found (#1)
    * Cambio del favicon y el tìtulo de la página
    
    * Creacion de la pagina principal y la pagina NotFound, se crea el store para trabajar ocn redux

---
+++ src/pages/MainView.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "../styles/MainView.css";
+
+function MainView() {
+  return (
+    <div className="MainView">
+      <div className="MainView-InfoBlock">
+        <h1>Comic</h1>
+        <h1>Rating</h1>
+        <h1>Button</h1>
+      </div>
+    </div>
+  );
+}
+
+export default MainView;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,14 @@
+import "../styles/NotFound.css";
+
+function NotFound() {
+  return (
+    <div className="Message">
+      <h1>
+        {" "}
+        Ooops! <br /> Page Not Found
+      </h1>
+    </div>
+  );
+}
+
+export default NotFound;
