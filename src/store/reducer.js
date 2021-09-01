2ea0eefe71b13beaf4fbf0f2d9e5587f48787f53
Natalia Bedoya Ruiz <44883548+NataliaBedoya@users.noreply.github.com>
2021-09-01 00:01:46 -0500

    Rama02 (#2)
    * Configuración de la petición del comic mediante el uso del proxy
    
    * Agrega la funcionalidad para visualizar el comic en el componente comic y crea el componente rating con el uso de la libreria react-icons
    
    * Agrega la funcionalidad del boton para renderizar la siguiente historia

---
+++ src/store/reducer.js
@@ -0,0 +1,38 @@
+import { getComic } from "./services/request";
+
+export const GET_COMIC_INFO = "GET_COMIC_INFO";
+
+const initialState = {
+  comic: {},
+};
+
+export function getComicInfo() {
+  const id = Math.floor(Math.random() * 2500) + 1;
+  return async function (dispatch) {
+    try {
+      const { data } = await getComic(id);
+      dispatch({
+        type: GET_COMIC_INFO,
+        payload: data,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+}
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_COMIC_INFO: {
+      return {
+        ...state,
+        comic: action.payload,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export default reducer;
--- src/store/services/request.js
+++ src/store/services/request.js
@@ -1,13 +1,9 @@
 import axios from "axios";
 
-export const getComic = async (id) => {
-  try {
-    const comic = await axios({
-      method: "GET",
-      url: `https://xkcd.com/${id}/info.0.json `,
-    });
-    return comic.data;
-  } catch (error) {
-    throw error;
-  }
-};
+export async function getComic(id) {
+  return await axios({
+    method: "GET",
+    baseURL: process.env.REACT_APP_BASE_URL,
+    url: `${id}`,
+  });
+}
